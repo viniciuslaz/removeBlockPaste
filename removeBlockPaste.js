0a345d17dc8df879e3fc1abdecef620e358469ee
viniciuslaz <viniciusg.lazzari@gmail.com>
2022-12-01 14:07:21 -0300

    permitir em qualquer huawei

--- removeBlockPaste.js
+++ removeBlockPaste.js
@@ -1,6 +1,6 @@
 disableEvents = (event => {
     let name = document.title;
-    if(name=="HUAWEI WiFi WS5200" || name=="HUAWEI WiFi AX2S" || name=="HUAWEI WiFi AX2"){
+    if(name.includes('HUAWEI')){
         event.stopImmediatePropagation();
     }
 });

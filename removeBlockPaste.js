f9d40821891f008e112dd281fae864ee6a9df146
Vinicius Lazzari <vininet17@gmail.com>
2023-03-09 23:57:08 -0300

    atualizado

--- removeBlockPaste.js
+++ removeBlockPaste.js
@@ -1,7 +1,20 @@
-disableEvents = (event => {
-    let name = document.title;
-    if(name.includes('HUAWEI')){
-        event.stopImmediatePropagation();
+window.onload = function () {
+    let namePage = document.title;
+
+    disableEvents = (event => {
+        if (namePage.includes('HUAWEI')) {
+            event.stopImmediatePropagation();
+        }
+    });
+    document.addEventListener('paste', disableEvents, true);
+
+    if (namePage.includes("Iniciar sessão")) {
+        let credenciais = document.getElementById("errmsg").innerText;
+        document.getElementById("username").value = "admin"
+
+        if(credenciais != "Credenciais inválidas"){
+            document.getElementById("password").value = "210802ac"
+            document.querySelector("[type=submit]").click();
+        }
     }
-});
-document.addEventListener('paste',disableEvents,true);
+}
